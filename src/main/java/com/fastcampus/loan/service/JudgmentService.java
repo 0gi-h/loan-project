b9a5cdfd3d102a535c5ab5702edca27c316b354d
gi0-h <hwicksky2@naver.com>
2024-05-24 22:50:55 +0900

    대출 심사 수정, 삭제, 한도 부여 기능

--- src/main/java/com/fastcampus/loan/service/JudgmentService.java
+++ src/main/java/com/fastcampus/loan/service/JudgmentService.java
@@ -1,5 +1,6 @@
 package com.fastcampus.loan.service;
 
+import com.fastcampus.loan.dto.ApplicationDTO;
 import com.fastcampus.loan.dto.JudgmentDTO;
 
 public interface JudgmentService {
@@ -11,4 +12,10 @@ public interface JudgmentService {
 
     //신청ID를 통한 조회
     JudgmentDTO.Response getJudgmentOfApplication(Long applicationId);
+
+    JudgmentDTO.Response update(Long judgmentId, JudgmentDTO.Request request);
+
+    void delete(Long judgmentId);
+
+    ApplicationDTO.GrantAmount grant(Long judgmentId);
 }
--- src/main/java/com/fastcampus/loan/service/JudgmentServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/JudgmentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.fastcampus.loan.service;
 
+import com.fastcampus.loan.domain.Application;
 import com.fastcampus.loan.domain.Judgment;
+import com.fastcampus.loan.dto.ApplicationDTO;
 import com.fastcampus.loan.dto.JudgmentDTO;
 import com.fastcampus.loan.exception.BaseException;
 import com.fastcampus.loan.exception.ResultType;
@@ -9,6 +11,8 @@ import com.fastcampus.loan.repository.JudgmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -57,6 +61,50 @@ public class JudgmentServiceImpl implements JudgmentService{
         });
 
         return modelMapper.map(judgment, JudgmentDTO.Response.class);
+    }
+
+    @Override
+    public JudgmentDTO.Response update(Long judgmentId, JudgmentDTO.Request request) {
+        Judgment judgment = judgmentRepository.findById(judgmentId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        judgment.setName(request.getName());
+        judgment.setApprovalAmount(request.getApprovalAmount());
+
+        judgmentRepository.save(judgment);
+
+        return modelMapper.map(judgment, JudgmentDTO.Response.class);
+    }
+
+    @Override
+    public void delete(Long judgmentId) {
+        Judgment judgment = judgmentRepository.findById(judgmentId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        judgment.setIsDeleted(true);
+
+        judgmentRepository.save(judgment);
+    }
+
+    @Override
+    public ApplicationDTO.GrantAmount grant(Long judgmentId) {
+        Judgment judgment = judgmentRepository.findById(judgmentId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        Long applicationId = judgment.getApplicationId();
+        Application application = applicationRepository.findById(applicationId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        BigDecimal approvalAmount = judgment.getApprovalAmount();
+        application.setApprovalAmount(approvalAmount);
+
+        applicationRepository.save(application);
+
+        return modelMapper.map(application, ApplicationDTO.GrantAmount.class);
     }
 
     private boolean isPressentApplication(Long applicationId) {
